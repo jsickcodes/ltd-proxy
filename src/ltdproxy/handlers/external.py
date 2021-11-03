f8bad487e8344b33b18fad88ae482e494088999d
Jonathan Sick <jonathan@jsick.codes>
2021-11-03 15:37:41 -0400

    Implement GitHub auth on the proxy handler

--- src/ltdproxy/handlers/external.py
+++ src/ltdproxy/handlers/external.py
@@ -4,7 +4,7 @@ from typing import Union
 
 import httpx
 from authlib.integrations.starlette_client import OAuthError
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from safir.dependencies.http_client import http_client_dependency
 from safir.dependencies.logger import logger_dependency
 from starlette.background import BackgroundTask
@@ -18,7 +18,10 @@ from structlog.stdlib import BoundLogger
 
 from ltdproxy.config import config
 from ltdproxy.githubauth import (
+    AuthResult,
+    GitHubAuth,
     GitHubOAuthType,
+    github_auth_dependency,
     github_oauth_dependency,
     set_serialized_github_memberships,
 )
@@ -30,7 +33,7 @@ external_router = APIRouter()
 """FastAPI router for all external handlers."""
 
 
-@external_router.get("/")
+@external_router.get("/", name="homepage")
 async def homepage(request: Request) -> HTMLResponse:
     github_token = request.session.get("github_token")
     if github_token:
@@ -39,7 +42,7 @@ async def homepage(request: Request) -> HTMLResponse:
     return HTMLResponse('<a href="/ltdproxy/login">login</a>')
 
 
-@external_router.get("/auth")
+@external_router.get("/auth", name="get_oauth_callback")
 async def get_oauth_callback(
     request: Request,
     logger: BoundLogger = Depends(logger_dependency),
@@ -58,7 +61,7 @@ async def get_oauth_callback(
     )
     if github_token:
         request.session["github_token"] = github_token
-    set_serialized_github_memberships(
+    await set_serialized_github_memberships(
         http_client=http_client,
         session=request.session,
         github_token=github_token,
@@ -66,7 +69,7 @@ async def get_oauth_callback(
     return RedirectResponse(url="/ltdproxy/")
 
 
-@external_router.get("/login")
+@external_router.get("/login", name="login")
 async def login(
     request: Request,
     logger: BoundLogger = Depends(logger_dependency),
@@ -78,37 +81,49 @@ async def login(
     return await github_oauth.authorize_redirect(request, redirect_uri)
 
 
-@external_router.get("/logout")
+@external_router.get("/logout", name="logout")
 async def logout(
     request: Request,
     logger: BoundLogger = Depends(logger_dependency),
 ) -> RedirectResponse:
     request.session.pop("github_token", None)
+    request.session.pop("github_memberships", None)
     logger.info("Logged out")
     return RedirectResponse(url="/ltdproxy/")
 
 
 @external_router.get(
-    "/{path:path}",
-    description="The S3 front-end proxy.",
+    "/{path:path}", description="The S3 front-end proxy.", name="proxy"
 )
 async def get_s3(
     path: str,
+    request: Request,
     logger: BoundLogger = Depends(logger_dependency),
     bucket: Bucket = Depends(bucket_dependency),
     http_client: httpx.AsyncClient = Depends(http_client_dependency),
-) -> StreamingResponse:
+    github_auth: GitHubAuth = Depends(github_auth_dependency),
+) -> Union[StreamingResponse, RedirectResponse]:
     """The S3 proxy endpoint."""
-    bucket_path = f"{config.s3_bucket_prefix}{path}"
-    stream = await bucket.stream_object(http_client, bucket_path)
-    logger.info("stream headers", headers=stream.headers)
-    response_headers = {
-        "Content-type": stream.headers["Content-type"],
-        "Content-length": stream.headers["Content-length"],
-        "Etag": stream.headers["Etag"],
-    }
-    return StreamingResponse(
-        stream.aiter_raw(),
-        background=BackgroundTask(stream.aclose),
-        headers=response_headers,
+    github_auth_result = github_auth.is_session_authorized(
+        path=f"/{path}", session=request.session
     )
+    if github_auth_result == AuthResult.unauthenticated:
+        return RedirectResponse(url=request.url_for("login"))
+    elif github_auth_result == AuthResult.unauthorized:
+        raise HTTPException(status_code=403, detail="Not authorized")
+    elif github_auth_result == AuthResult.authorized:
+        bucket_path = f"{config.s3_bucket_prefix}{path}"
+        stream = await bucket.stream_object(http_client, bucket_path)
+        logger.info("stream headers", headers=stream.headers)
+        response_headers = {
+            "Content-type": stream.headers["Content-type"],
+            "Content-length": stream.headers["Content-length"],
+            "Etag": stream.headers["Etag"],
+        }
+        return StreamingResponse(
+            stream.aiter_raw(),
+            background=BackgroundTask(stream.aclose),
+            headers=response_headers,
+        )
+    else:
+        raise HTTPException(status_code=500, detail="Internal auth error")
